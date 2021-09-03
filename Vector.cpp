1ed909cc861e609c0f825a6e1c829208bbff0eae
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-09-03 21:37:12 +0530

    Create Vector.cpp

---
+++ Vector.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   /* vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+    vector<int> v2;
+    int n,a;
+    cin>>n;
+    for(int i=0;i<n;i++){
+            cin>>a;
+        v2.push_back(a);
+    }
+    for(int i=0;i<v2.size();i++){
+        cout<<v2[i]<<" ";
+    }
+    cout<<endl;
+
+    vector<int> v3;
+    v3.push_back(11);
+    v3.push_back(22);
+    v3.push_back(33);
+    vector<int>::iterator it;
+    for(it=v3.begin();it!=v3.end();it++){
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+    */
+    vector<int> v4;
+    v4.push_back(11);
+    v4.push_back(22);
+    v4.push_back(33);
+    for(auto i:v4){
+        cout<<v4<<" ";
+    }
+
+    return 0;
+}
+
