3a947a559ece324ce09843a46927a4067f8bd5ec
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-09-04 12:31:03 +0530

    Create Nested_vector.cpp

---
+++ Nested_vector.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(vector<int> &p){
+    cout<<"size is:"<<p.size()<<" ";
+    for(int i=0;i<p.size();i++){
+        cout<<p[i]<<" ";
+    }
+    cout<<endl;
+    return;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v[n];
+    for(int i=0;i<n;i++){
+        int s;
+        cin>>s;
+        for(int j=0;j<s;j++){
+            int a;
+            cin>>a;
+            v[i].push_back(a);
+        }
+    }
+    for(int i=0;i<n;i++){
+        print(v[i]);
+    }
+
+    return 0;
+}
