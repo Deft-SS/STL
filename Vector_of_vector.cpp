343a20ea04de0b99d143e1674305c640dddfc2eb
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-09-13 23:27:12 +0530

    Update Vector_of_vector.cpp

--- Vector_of_vector.cpp
+++ Vector_of_vector.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void print(vector<int> &p){
     cout<<"size is:"<<p.size()<<" ";
     for(int i=0;i<p.size();i++){
-        cout<<p[i]<<" ";
+        cout<<p[i]<<" " ;
     }
     cout<<endl;
     return;
